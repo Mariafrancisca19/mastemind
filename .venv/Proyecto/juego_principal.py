c6c7ca21461387334923fba0de4fd502307166ba
Mariafrancisca19 <franciscasaborio19@gmail.com>
2024-08-06 11:12:30 -0600

    creacion de clase juego principal y validacion ya sea el jugador o la maquina jueguen

--- .venv/Proyecto/juego_principal.py
+++ .venv/Proyecto/juego_principal.py
@@ -1,7 +1,41 @@
 from tablero import Tablero
-from jugador import Jugador
+from jugador import Jugador,Adivina,CreaCodigo
 
 
 class JuegoPrincipal:
+    def __init__(self) -> None:
+        self.tablero = Tablero()
+        self.adivina = None
+        self.crea_codigo = None
+        
+        
+    # metodo inicializacion del juego
     
+    def inicio(self):
+        jugador_input = input("Eres creador o adivinador")
+        
+        if jugador_input == 'creador':
+         self.crea_codigo = CreaCodigo(persona=True)
+         self.adivina = Adivina(persona=False)
+        elif jugador_input == 'adivinador':
+            self.crea_codigo = CreaCodigo(persona=False)
+            self.adivina = Adivina(persona=True)
+        else: 
+            print("Error digite correctamnte palabra")
+            return 
+        self.tablero.definir_color(self.crea_codigo.obtener_codigo())
+        
+    def juego(self):
+        if self.crea_codigo is None or self.adivina is None:
+            print("defina jugador")
+            return
+        
+        for i in range(12):
+            intento = self.adivina.obtener_codigo()
+            retroalimentacion = self.tablero.validar_ganador(intento)
+            self.tablero.actualiza(intento,retroalimentacion)
+            self.tablero.mostrar()
+            
+            
+        
 
